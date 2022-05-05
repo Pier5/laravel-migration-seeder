9756e1883cd6e161db640efbfb670577d9456ba8
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-05 18:00:43 +0200

    -aggiunto controllo anche sull'orario, quindi togliendo i treni già partiti anche se in data odierna, col whereTime-

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,6 +11,10 @@ class PageController extends Controller
         // in partenza oggi
         $trainsList = Train::whereDate('departure_time', date('Y-m-d'))->orderBy('departure_time', 'asc')->get(); 
 
+        // controllo anche sull'orario
+        // $trainsList = Train::whereDate('departure_time', date('Y-m-d'))->whereTime('departure_time', '>=', date('H:i:s'))->orderBy('departure_time', 'asc')->get(); 
+
+
         // in partenza questo mese
         // $trainsList = Train::whereMonth('departure_time', date('m'))->orderBy('departure_time', 'asc')->get(); 
 
