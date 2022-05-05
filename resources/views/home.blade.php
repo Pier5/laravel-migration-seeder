24bff89e8ed7ba5123a57f7f51896e1ddf40dc91
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-05 16:05:27 +0200

    -passato solo un treno per ora, prova stampa su schermo-

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,20 @@
 @section('title', 'Train timetables')
 
 @section('content')
-   
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Train timetables</title>
+   </head>
+   <body>
+      <main>
+         @foreach($trainsList as $train)
+            <h1>{{ $train->company }}</h1>
+         @endforeach
+      </main>
+   </body>
+   </html>
 @endsection
