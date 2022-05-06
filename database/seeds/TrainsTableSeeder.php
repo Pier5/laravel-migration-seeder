c04dcbd9cf6c66859c224c758aea6e44d60623fe
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-06 15:30:54 +0200

    -db popolato con faker-

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -11,32 +11,53 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $arrTrains = [
-            [
-                'company' => 'Trenitalia',
-                'departure_station' =>'Olbia',
-                'arrival_station' =>'Sassari',
-                'departure_time' =>'2022-05-06 12:00',
-                'arrival_time' =>'2022-05-06 13:10'
-            ],
-            [
-                'company' => 'Trenitalia',
-                'departure_station' =>'Bergamo',
-                'arrival_station' =>'Milano',
-                'departure_time' =>'2022-05-06 12:00',
-                'arrival_time' =>'2022-05-06 13:00'
-            ]
-        ];
 
-        foreach($arrTrains as $trainData) {
+
+        // Senza faker
+
+        // $arrTrains = [
+        //     [
+        //         'company' => 'Trenitalia',
+        //         'departure_station' =>'Olbia',
+        //         'arrival_station' =>'Sassari',
+        //         'departure_time' =>'2022-05-06 12:00',
+        //         'arrival_time' =>'2022-05-06 13:10'
+        //     ],
+        //     [
+        //         'company' => 'Trenitalia',
+        //         'departure_station' =>'Bergamo',
+        //         'arrival_station' =>'Milano',
+        //         'departure_time' =>'2022-05-06 12:00',
+        //         'arrival_time' =>'2022-05-06 13:00'
+        //     ]
+        // ];
+
+        // foreach($arrTrains as $trainData) {
+        //     $train = new Train();
+        //     // $train->company = $trainData['company'];
+        //     // $train->company = $trainData['departure_station'];
+        //     // $train->company = $trainData['arrival_station'];
+        //     // $train->company = $trainData['departure_time'];
+        //     // $train->company = $trainData['arrival_time'];
+        //     $train->fill($trainData);
+        //     $train->save();
+        // }
+
+
+
+        // Con faker
+
+        for($i = 0; $i < 200; $i++) {
+            $trainData = [
+                'company'           => $faker->company() ,
+                'departure_station' => $faker->city(),
+                'arrival_station'   => $faker->city(),
+                'departure_time'    => $faker->dateTimeBetween('now', '+1 week'),
+                'arrival_time'      => $faker->dateTimeBetween('now', '+1 week'),
+            ];
             $train = new Train();
-            // $train->company = $trainData['company'];
-            // $train->company = $trainData['departure_station'];
-            // $train->company = $trainData['arrival_station'];
-            // $train->company = $trainData['departure_time'];
-            // $train->company = $trainData['arrival_time'];
             $train->fill($trainData);
             $train->save();
         }
